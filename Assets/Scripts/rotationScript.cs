bb75048d3040959e907e30a3cdbb4b4742ca775e
Kweku Krampah <90955686+KnKrampah@users.noreply.github.com>
2023-08-23 11:20:37 +0000

    22/08/2023

---
+++ Assets/Scripts/rotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationScript : MonoBehaviour
+{
+    public float rotationSpeed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+    // Rotate the object around its up (Y) axis
+    transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+}
+}
